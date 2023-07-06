50899c2b9014acb19ad9447747b788053122cdee
Arne Wäschenbach <arne.waeschenbach@gmail.com>
2023-07-06 21:37:40 +0200

    ?

--- App/vite.config.js
+++ App/vite.config.js
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig({
+const path = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
   build: {
-    outDir: 'dist',
+    outDir: path.resolve(__dirname, 'dist'), // Ausgabeverzeichnis für den Build
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/main.jsx') // Einstiegsdatei für den Build
+      },
+      output: {
+        entryFileNames: 'my-lib.js', // Name der generierten Datei
+        format: 'umd', // Format der generierten Datei
+        name: 'MyLib', // Name der exportierten Bibliothek
+        globals: {
+          react: 'React' // Globale Variable für React-Abhängigkeit
+        }
+      }
+    }
   },
-  plugins: [react()],
+  plugins: [react()] // React-Plugin hinzufügen
 })
